57bca0c09c959a3c9eea4952692bbc40c341a281
Willow <thelivingviking777@gmail.com>
2026-05-01 17:46:22 +0530

    Added basic server.js

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+
+//for security purpose
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+
+//for keeping the API keys and other secrets safe
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minutes
+    max: 60, // limit each IP to 60 requests per windowMs
+  }),
+);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+//Global error handler
+const errorhandler = (err, req, res, next) => {
+  const status_code = err.status_code || 500;
+
+  const message = status_code === 500 ? "Internal Server Error" : err.message;
+
+  res.status(status_code).json({
+    success: false,
+    message: message,
+  });
+};
+
+app.use(errorhandler);
+
+app.get("/error", (req, res, next) => {
+  const err = new Error("Test error");
+  err.status_code = 400;
+  next(err);
+});
